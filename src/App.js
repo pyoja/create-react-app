4913b9dbfc76d9e3a734e74c1c5f3629354a827e
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-10 17:50:55 +0900

    개별 좋아요 적용

--- src/App.js
+++ src/App.js
@@ -2,35 +2,40 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목, b] = useState(["남자코트", "여자코트", "사장코드"]);
-  let [like, setLike] = useState(0);
+  let [글제목, b] = useState(["남자코트", "여자코트", "사장코트"]);
+  let [likes, setLikes] = useState([0, 0, 0]);
+
+  const increaseLikes = (index) => {
+    const newLikes = [...likes];
+    newLikes[index] = newLikes[index] + 1;
+    setLikes(newLikes);
+  };
 
   return (
     <div className="App">
       <div className="balck-nav">
         <h4>표정훈 블로그</h4>
       </div>
-      <div className="list">
-        <h4>
-          {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              setLike(like + 1);
-            }}
-          >
-            👍
-          </span>
-          {like}
-        </h4>
-        <p>발행일:23-07-10</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>발행일:23-07-10</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>발행일:23-07-10</p>
+      {글제목.map((title, index) => (
+        <div className="list" key={index}>
+          <h4>
+            {title}
+            <span
+              onClick={() => {
+                increaseLikes(index);
+              }}
+            >
+              👍
+            </span>
+            {likes[index]}
+          </h4>
+          <p>발행일:23-07-10</p>
+        </div>
+      ))}
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>내용</p>
       </div>
     </div>
   );
