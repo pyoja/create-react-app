d344ad107cd45b3f92d6b96d36a1d1dbf403cc0d
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-10 16:19:31 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [글제목, b] = useState(["남자코트", "여자코트", "사장코드"]);
+
   return (
     <div className="App">
       <div className="balck-nav">
         <h4>표정훈 블로그</h4>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{글제목[0]}</h4>
+        <p>발행일:23-07-10</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>발행일:23-07-10</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>발행일:23-07-10</p>
       </div>
     </div>
